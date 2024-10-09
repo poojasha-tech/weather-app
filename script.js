aff61dcc6971137bb3a2686abc14d062e949418e
jaipooja <jaipooja199@gmail.com>
2024-10-09 22:15:10 +0200

    complete project

---
+++ script.js
@@ -0,0 +1,63 @@
+const apiKey="8c5e7f237f99c25f76c764900bb10624";
+const apiURL="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox=document.querySelector(".search input");
+const searchBtn=document.querySelector(".search button");
+const weatherIcon=document.querySelector(".weather-icon");
+
+ async function checkWeather(city) {
+
+    const response=await fetch(apiURL+ city +`&appid=${apiKey}`);
+
+    if(response.status==404){
+        document.querySelector(".error").style.display="block";
+        document.querySelector(".weather").style.display="none";
+    }
+    else{
+        var data=await response.json();
+
+    console.log(data);
+
+    
+    document.querySelector(".city").innerHTML=data.name;
+    document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+"°C";
+    document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
+    document.querySelector(".wind").innerHTML=data.wind.speed+"km/hr";
+
+    if(data.weather[0].main=="Clouds"){
+        weatherIcon.src="images/clouds.png";
+    }
+    else if (data.weather[0].main=="Clear"){
+        weatherIcon.src="images/clear.png";
+    }
+    else if (data.weather[0].main=="Drizzle"){
+        weatherIcon.src="images/drizzle.png";
+    }
+    else if (data.weather[0].main=="Rain"){
+        weatherIcon.src="images/rain.png";
+    }
+    else if (data.weather[0].main=="Mist"){
+        weatherIcon.src="images/mist.png";
+    }
+    else if (data.weather[0].main=="Snow"){
+        weatherIcon.src="images/snow.png";
+    }
+
+    document.querySelector(".weather").style.display="block";
+    document.querySelector(".error").style.display="none";
+    }
+    
+} 
+ searchBtn.addEventListener("click",()=>{
+    checkWeather(searchBox.value); 
+})
+searchBox.addEventListener("keypress",function (e){
+    console.log(e);
+     if (e.charCode==13){
+        debugger;
+        checkWeather(searchBox.value);
+    }
+})
+const print = (a)=>{
+  console.log(a)
+}
